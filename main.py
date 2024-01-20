a29c842989d0f3401bc2afa73d91e5b40d0c4dcd
ruslanchik <rak.geter@gmail.com>
2024-01-20 16:39:14 +0300

    add slider

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from aiogram import Bot, Dispatcher, types
+
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
 from parser import parser, search_parser
 import asyncio
 
@@ -7,20 +12,87 @@ bot = Bot(token='6989175958:AAFah4VCpvTE_1lj6rsCxAeUd51nmps5ZLo')
 dp = Dispatcher(bot=bot)
 
 
+class OrderFood(StatesGroup):
+    count = State()
+
+
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    await message.answer('Для выдачи порно введи слово "/random"\nДля поиска вводи /search [запрос]')
+    kb = [
+        [
+            types.KeyboardButton(text="С пюрешкой"),
+            types.KeyboardButton(text="Без пюрешки")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    await message.answer('Для выдачи порно введи слово "/random"\nДля поиска вводи /search [запрос]',
+                         reply_markup=keyboard)
+
+
+@dp.message(F.text == 'hi')
+async def hello(message: types.Message):
+    await message.answer('Привет')
 
 
 @dp.message(Command("random"))
-async def cmd_test1(message: types.Message):
+async def cmd_random(message: types.Message):
     await message.reply(parser())
 
 
 @dp.message(Command("search"))
-async def cmd_test1(message: types.Message):
+async def cmd_test2(message: types.Message, state: FSMContext):
+    await state.update_data(index=0)
     user_request = message.text.split(' ')[1:]
-    await message.reply(search_parser('+'.join(user_request)))
+    await state.update_data(videos=search_parser('+'.join(user_request)))
+    data = await state.get_data()
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text=">",
+        callback_data="next")
+    )
+    await message.answer(
+        str(data['videos'][0]),
+        reply_markup=builder.as_markup()
+    )
+
+
+@dp.callback_query(F.data == "back")
+async def back_video(callback: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    builder = InlineKeyboardBuilder()
+    if data["index"] != 0:
+        await state.update_data(index=data['index'] - 1)
+    updated_data = await state.get_data()
+    builder.add(types.InlineKeyboardButton(
+        text="<",
+        callback_data="back"
+    )).add(types.InlineKeyboardButton(
+        text=">",
+        callback_data="next"
+    ))
+    await callback.message.edit_text(str(updated_data['videos'][updated_data['index']]), reply_markup=builder.as_markup())
+    await callback.answer()
+
+
+@dp.callback_query(F.data == "next")
+async def next_video(callback: types.CallbackQuery, state: FSMContext):
+    builder = InlineKeyboardBuilder()
+    data = await state.get_data()
+    await state.update_data(index=data['index'] + 1)
+    updated_data = await state.get_data()
+    builder.add(types.InlineKeyboardButton(
+        text="<",
+        callback_data="back"
+    )).add(types.InlineKeyboardButton(
+        text=">",
+        callback_data="next"
+    ))
+    await callback.message.edit_text(str(updated_data['videos'][updated_data['index']]), reply_markup=builder.as_markup())
+    await callback.answer()
 
 
 async def main():
--- parser.py
+++ parser.py
@@ -11,10 +11,14 @@ def parser():
 
 
 def search_parser(search):
+    arr = []
     request = requests.get(f'https://rt.pornhub.com/video/search?search={search}&page={1}')
     soap = BeautifulSoup(request.text, 'html.parser')
     all_links = soap.findAll('a', class_='linkVideoThumb')
-    return 'https://rt.pornhub.com' + all_links[random.choice(range(1, 15))].get('href')
+    for link in all_links:
+        arr.append('https://rt.pornhub.com' + link.get('href'))
+    print(len(arr))
+    return arr[4: 30]
 
 
 
